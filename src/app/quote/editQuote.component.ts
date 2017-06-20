1a44154eb9c6636e3b40f0440778e5f86301d125
alan345 <alan345@gmail.com>
2017-06-20 17:30:39 +0200

    remove item

--- src/app/quote/editQuote.component.ts
+++ src/app/quote/editQuote.component.ts
@@ -151,6 +151,7 @@ export class EditQuoteComponent implements OnInit {
     }
     removeProduct(i: number) {
       this.fetchedQuote.devisDetails.splice(i, 1);
+      this.calculateQuote()
     }
 
     getProducts(page: number, search: any) {
