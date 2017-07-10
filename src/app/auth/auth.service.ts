2913af66473d84991bdcd6080498e6028bdfcf88
alan345 <alan345@gmail.com>
2017-07-10 10:31:20 -0700

    ok

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -104,7 +104,7 @@ export class AuthService {
 
 
   getCurrentUser(){
-    console.log(localStorage.getItem('id_token') )
+    // console.log(localStorage.getItem('id_token') )
     let userInfo = localStorage.getItem('id_token') ? this.jwtHelper.decodeToken(localStorage.getItem('id_token')) : null;
     console.log(userInfo)
     return userInfo.user
