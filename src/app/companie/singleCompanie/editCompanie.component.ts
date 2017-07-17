b5220718e874ed6addfc2437f9f2899c422ede54
alan345 <alan345@gmail.com>
2017-07-17 12:34:50 -0700

    categories project in companie

--- src/app/companie/singleCompanie/editCompanie.component.ts
+++ src/app/companie/singleCompanie/editCompanie.component.ts
@@ -83,15 +83,31 @@ export class EditCompanieComponent implements OnInit {
     })
   }
 
-  addCategProject(level, index1, index2) {
-    let newCategorie = new Categorie0()
-    if(level === 0)
-      this.fetchedCompanie.categories.categProject.push(newCategorie)
-    if(level === 1)
-      this.fetchedCompanie.categories[index1].subCateg.push(newCategorie)
-    if(level === 2)
-      this.fetchedCompanie.categories[index1].subCateg[index2].subCateg.push(newCategorie)
+  addCategProject(type, level, index1, index2, index3) {
+    if(type==='project'){
+      let newCategorie = new Categorie0()
+      if(level === 0)
+        this.fetchedCompanie.categories.categProject.unshift(newCategorie)
+      if(level === 1)
+        this.fetchedCompanie.categories.categProject[index1].subCateg.unshift(newCategorie)
+      if(level === 2)
+        this.fetchedCompanie.categories.categProject[index1].subCateg[index2].subCateg.unshift(newCategorie)
+    }
   }
+
+  removeCategProject(type, level, index1, index2, index3) {
+    if(type==='project') {
+      if(level === 0)
+        this.fetchedCompanie.categories.categProject.splice(level, 1)
+      if(level === 1)
+        this.fetchedCompanie.categories.categProject.splice(index1, 1)
+      if(level === 2)
+        this.fetchedCompanie.categories.categProject[index1].subCateg.splice(index2, 1)
+      if(level === 3)
+        this.fetchedCompanie.categories.categProject[index1].subCateg[index2].subCateg.splice(index3, 1)
+    }
+  }
+
 
   fetchedCurrentUser: User = new User()
   getCurrentUser() {
@@ -128,240 +144,240 @@ export class EditCompanieComponent implements OnInit {
   // }
 
   save() {
-     this.fetchedCompanie.categories.categProject = [
-  {
-    "categ":"Devis Rénovation",
-    "subCateg": [
-      {
-        "categ":"Cuisine",
-        "subCateg": [
-          {"categ": "ok"}
-        ]
-      },
-      {
-        "categ":"Salle de Bain",
-        "subCateg": [
-          {"categ": "ok"}
-        ]
-      },
-      {
-        "categ":"Remise en conformité électrique",
-        "subCateg": [
-          {"categ": "ok"}
-        ]
-      },
-      {
-        "categ":"Maçonnerie",
-        "subCateg": [
-          {"categ": "ok"}
-        ]
-      },
-      {
-        "categ":"Peinture",
-        "subCateg": [
-          {"categ": "ok"}
-        ]
-      },
-      {
-        "categ":"PMR",
-        "subCateg": [
-          {"categ": "ok"}
-        ]
-      }
-    ]
-  },
-
-
-
-
-  {
-    "categ":"Intervention Immédiate",
-    "subCateg": [
-
-      {
-        "categ":"Plomberie",
-        "subCateg": [
-          {"categ": "Recherche de Fuite"},
-          {"categ": "Réparation De Fuite"},
-          {"categ": "Engorgement de canalisation"},
-          {"categ": "Ballon Eau chaude en panne"},
-          {"categ": "Robinneterie en panne"},
-          {"categ": "Wc en panne"},
-          {"categ": "Innondation"}
-        ]
-      },
-      {
-        "categ":"Electricité",
-        "subCateg": [
-          {"categ": "Recherche de panne électrique"},
-          {"categ": "Remplacement de tableau électrique "}
-        ]
-      },
-      {
-        "categ":"Serrurerie",
-        "subCateg": [
-          {"categ": "ouverture de porte"},
-          {"categ": "Remplacement de serrure"}
-        ]
-      },
-      {
-        "categ":"Vitrerie",
-        "subCateg": [
-          {"categ": "Remplacement de vitre à la coupe"},
-          {"categ": "Fermeture Provisoire"}
-        ]
-      },
-      {
-        "categ":"Chauffage",
-        "subCateg": [
-          {"categ": "Fuite Chaudiere ou Radiateur"},
-          {"categ": "Recherche de panne chaudiere"},
-          {"categ": "Contrat Entretien"},
-          {"categ": "Remplacement de Chaudiere "}
-        ]
-      }
-    ]
-  },
-
-
-
-
-  {
-    "categ":"Devis Installation",
-    "subCateg": [
-
-      {
-        "categ":"Menuiserie",
-        "subCateg": [
-          {"categ": "Double Vitrage rénovation"},
-          {"categ": "Vitre / Vitrine"},
-          {"categ": "Fenêtres"},
-          {"categ": "Survitrage"}
-        ]
-      },
-      {
-        "categ":"Chauffage",
-        "subCateg": [
-          {"categ": "Remplacement de chaudiere"},
-          {"categ": "Déplacement de chaudiere"},
-          {"categ": "Installation de radiateur"}
-        ]
-      },
-      {
-        "categ":"Petite Electricité",
-        "subCateg": [
-          {"categ": "Tirage de ligne électrique"},
-          {"categ": "Rajout de prise"}
-        ]
-      },
-      {
-        "categ":"Serrurerie",
-        "subCateg": [
-          {"categ": "Installation d\"une serrure de sécurité"},
-          {"categ": "Blindage de porte"},
-          {"categ": "Remplacement de porte"}
-        ]
-      },
-      {
-        "categ":"Plomberie",
-        "subCateg": [
-          {"categ": "Remplacement de Ballon eau chaude"},
-          {"categ": "Remplacement de robinneterie"},
-          {"categ": "Remplacement de WC"},
-          {"categ": "Tirage de ligne "}
-        ]
-      },
-      {
-        "categ":"Electricité",
-        "subCateg": [
-          {"categ": "Installation ou Remplacement Convecteur"}
-
-        ]
-      }
-    ]
-  },
-
-
-
-
-
-  {
-    "categ":"Électricité",
-    "subCateg": [
-
-      {
-        "categ":"Convecteur",
-        "subCateg": [
-          {"categ": "Convecteur électrique CASSETTE RAY SUNAIR 3600TC Thermor, ref : 497091"},
-          {"categ": "Convecteur électrique Thermor Equateur 2000W"},
-          {"categ": "Convecteur gamme Baleares 1500W horizontal tout compris"}
-        ]
-      },
-      {
-        "categ":"Disjoncteur",
-        "subCateg": [
-          {"categ": "Disjoncteur 10A"},
-          {"categ": "Disjoncteur 16A/ 30Mili"},
-          {"categ": "Disjoncteur 20A"},
-          {"categ": "Disjoncteur 32A"},
-          {"categ": "Disjoncteur C2"},
-          {"categ": "Disjoncteur Divers tri"}
-        ]
-      },
-      {
-        "categ":"Eclairage",
-        "subCateg": [
-          {"categ": "Interrupteur"},
-          {"categ": "Projecteurs"},
-          {"categ": "Lampes"}
-        ]
-      },
-      {
-        "categ":"Branchement",
-        "subCateg": [
-          {"categ": "Fils et câbles"},
-          {"categ": "Prises"}
-        ]
-      },
-      {
-        "categ":"Protection",
-        "subCateg": [
-          {"categ": "Protections de l\"habitat"},
-          {"categ": "Cartouches et fusibles"}
-        ]
-      }
-
-    ]
-  },
-
-
-  {
-    "categ": "Chauffage",
-    "subCateg": [
-
-      {
-        "categ": "Radiateurs",
-        "subCateg": [
-          {"categ": "Fixations de radiateurs"},
-          {"categ": "Radiateur thermoactif"},
-          {"categ": "Robinetterie de radiateurs"},
-          {"categ": "Accessoires"},
-          {"categ": "Sèche-serviettes"},
-          {"categ": "Habillage"}
-        ]
-      },
-      {
-        "categ":"Chaudière",
-        "subCateg": [
-          {"categ": "Bloc hydraulique"},
-          {"categ": "Système de sécurité surchauffe"},
-          {"categ": "Chaudière électrique"},
-          {"categ": "Thermostat"}
-        ]
-      }
-    ]
-  }
-]
+//      this.fetchedCompanie.categories.categProject = [
+//   {
+//     "categ":"Devis Rénovation",
+//     "subCateg": [
+//       {
+//         "categ":"Cuisine",
+//         "subCateg": [
+//           {"categ": "ok"}
+//         ]
+//       },
+//       {
+//         "categ":"Salle de Bain",
+//         "subCateg": [
+//           {"categ": "ok"}
+//         ]
+//       },
+//       {
+//         "categ":"Remise en conformité électrique",
+//         "subCateg": [
+//           {"categ": "ok"}
+//         ]
+//       },
+//       {
+//         "categ":"Maçonnerie",
+//         "subCateg": [
+//           {"categ": "ok"}
+//         ]
+//       },
+//       {
+//         "categ":"Peinture",
+//         "subCateg": [
+//           {"categ": "ok"}
+//         ]
+//       },
+//       {
+//         "categ":"PMR",
+//         "subCateg": [
+//           {"categ": "ok"}
+//         ]
+//       }
+//     ]
+//   },
+//
+//
+//
+//
+//   {
+//     "categ":"Intervention Immédiate",
+//     "subCateg": [
+//
+//       {
+//         "categ":"Plomberie",
+//         "subCateg": [
+//           {"categ": "Recherche de Fuite"},
+//           {"categ": "Réparation De Fuite"},
+//           {"categ": "Engorgement de canalisation"},
+//           {"categ": "Ballon Eau chaude en panne"},
+//           {"categ": "Robinneterie en panne"},
+//           {"categ": "Wc en panne"},
+//           {"categ": "Innondation"}
+//         ]
+//       },
+//       {
+//         "categ":"Electricité",
+//         "subCateg": [
+//           {"categ": "Recherche de panne électrique"},
+//           {"categ": "Remplacement de tableau électrique "}
+//         ]
+//       },
+//       {
+//         "categ":"Serrurerie",
+//         "subCateg": [
+//           {"categ": "ouverture de porte"},
+//           {"categ": "Remplacement de serrure"}
+//         ]
+//       },
+//       {
+//         "categ":"Vitrerie",
+//         "subCateg": [
+//           {"categ": "Remplacement de vitre à la coupe"},
+//           {"categ": "Fermeture Provisoire"}
+//         ]
+//       },
+//       {
+//         "categ":"Chauffage",
+//         "subCateg": [
+//           {"categ": "Fuite Chaudiere ou Radiateur"},
+//           {"categ": "Recherche de panne chaudiere"},
+//           {"categ": "Contrat Entretien"},
+//           {"categ": "Remplacement de Chaudiere "}
+//         ]
+//       }
+//     ]
+//   },
+//
+//
+//
+//
+//   {
+//     "categ":"Devis Installation",
+//     "subCateg": [
+//
+//       {
+//         "categ":"Menuiserie",
+//         "subCateg": [
+//           {"categ": "Double Vitrage rénovation"},
+//           {"categ": "Vitre / Vitrine"},
+//           {"categ": "Fenêtres"},
+//           {"categ": "Survitrage"}
+//         ]
+//       },
+//       {
+//         "categ":"Chauffage",
+//         "subCateg": [
+//           {"categ": "Remplacement de chaudiere"},
+//           {"categ": "Déplacement de chaudiere"},
+//           {"categ": "Installation de radiateur"}
+//         ]
+//       },
+//       {
+//         "categ":"Petite Electricité",
+//         "subCateg": [
+//           {"categ": "Tirage de ligne électrique"},
+//           {"categ": "Rajout de prise"}
+//         ]
+//       },
+//       {
+//         "categ":"Serrurerie",
+//         "subCateg": [
+//           {"categ": "Installation d\"une serrure de sécurité"},
+//           {"categ": "Blindage de porte"},
+//           {"categ": "Remplacement de porte"}
+//         ]
+//       },
+//       {
+//         "categ":"Plomberie",
+//         "subCateg": [
+//           {"categ": "Remplacement de Ballon eau chaude"},
+//           {"categ": "Remplacement de robinneterie"},
+//           {"categ": "Remplacement de WC"},
+//           {"categ": "Tirage de ligne "}
+//         ]
+//       },
+//       {
+//         "categ":"Electricité",
+//         "subCateg": [
+//           {"categ": "Installation ou Remplacement Convecteur"}
+//
+//         ]
+//       }
+//     ]
+//   },
+//
+//
+//
+//
+//
+//   {
+//     "categ":"Électricité",
+//     "subCateg": [
+//
+//       {
+//         "categ":"Convecteur",
+//         "subCateg": [
+//           {"categ": "Convecteur électrique CASSETTE RAY SUNAIR 3600TC Thermor, ref : 497091"},
+//           {"categ": "Convecteur électrique Thermor Equateur 2000W"},
+//           {"categ": "Convecteur gamme Baleares 1500W horizontal tout compris"}
+//         ]
+//       },
+//       {
+//         "categ":"Disjoncteur",
+//         "subCateg": [
+//           {"categ": "Disjoncteur 10A"},
+//           {"categ": "Disjoncteur 16A/ 30Mili"},
+//           {"categ": "Disjoncteur 20A"},
+//           {"categ": "Disjoncteur 32A"},
+//           {"categ": "Disjoncteur C2"},
+//           {"categ": "Disjoncteur Divers tri"}
+//         ]
+//       },
+//       {
+//         "categ":"Eclairage",
+//         "subCateg": [
+//           {"categ": "Interrupteur"},
+//           {"categ": "Projecteurs"},
+//           {"categ": "Lampes"}
+//         ]
+//       },
+//       {
+//         "categ":"Branchement",
+//         "subCateg": [
+//           {"categ": "Fils et câbles"},
+//           {"categ": "Prises"}
+//         ]
+//       },
+//       {
+//         "categ":"Protection",
+//         "subCateg": [
+//           {"categ": "Protections de l\"habitat"},
+//           {"categ": "Cartouches et fusibles"}
+//         ]
+//       }
+//
+//     ]
+//   },
+//
+//
+//   {
+//     "categ": "Chauffage",
+//     "subCateg": [
+//
+//       {
+//         "categ": "Radiateurs",
+//         "subCateg": [
+//           {"categ": "Fixations de radiateurs"},
+//           {"categ": "Radiateur thermoactif"},
+//           {"categ": "Robinetterie de radiateurs"},
+//           {"categ": "Accessoires"},
+//           {"categ": "Sèche-serviettes"},
+//           {"categ": "Habillage"}
+//         ]
+//       },
+//       {
+//         "categ":"Chaudière",
+//         "subCateg": [
+//           {"categ": "Bloc hydraulique"},
+//           {"categ": "Système de sécurité surchauffe"},
+//           {"categ": "Chaudière électrique"},
+//           {"categ": "Thermostat"}
+//         ]
+//       }
+//     ]
+//   }
+// ]
     //this.fetchedCompanie.categJson.categProduct = JSON.stringify(JSON.parse(this.fetchedCompanie.categJson.categProduct))
     if(this.fetchedCompanie._id) {
       this.companieService.updateCompanie(this.fetchedCompanie)
