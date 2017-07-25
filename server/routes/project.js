82dbcd8beb9710654a4503dbd443c19b9bf63027
alan345 <alan345@gmail.com>
2017-07-24 17:50:08 -0700

    clean

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,55 +1,52 @@
 var express = require('express'),
-    router  = express.Router(),
-    config  = require('../config/config'),
-    User    = require('../models/user.model'),
-    Project    = require('../models/project.model'),
-    Form    = require('../models/form.model'),
-    fs      = require('fs'),
-    jwt     = require('jsonwebtoken'),
-    shared = require('./shared.js'),
-    nameObject = 'project'
+  router = express.Router(),
+  config = require('../config/config'),
+  User = require('../models/user.model'),
+  Project = require('../models/project.model'),
+  Form = require('../models/form.model'),
+  fs = require('fs'),
+  jwt = require('jsonwebtoken'),
+  shared = require('./shared.js'),
+  nameObject = 'project'
 
 // this process does not hang the nodejs server on error
-process.on('uncaughtException', function (err) {
+process.on('uncaughtException', function(err) {
   console.log(err)
 })
 
-router.use('/', function (req, res, next) {
+router.use('/', function(req, res, next) {
   var token = req.headers['authorization']
-  jwt.verify(token, config.secret, function (err, decoded) {
+  jwt.verify(token, config.secret, function(err, decoded) {
     if (err) {
-      return res.status(401).json({
-        message: 'Authentication failed',
-        error: err
-      })
+      return res.status(401).json({message: 'Authentication failed', error: err})
     }
     if (!decoded) {
       return res.status(404).json({
         title: 'Authentication Failed',
-        error: {message: 'Authentication failed, malformed jwt'}
+        error: {
+          message: 'Authentication failed, malformed jwt'
+        }
       })
     }
     if (decoded) {
-      User
-      .findById(decoded.user._id)
-      .populate({ path: 'rights', model: 'Right'})
-      .exec(function (err, doc) {
+      User.findById(decoded.user._id).populate({path: 'rights', model: 'Right'}).exec(function(err, doc) {
         if (err) {
-          return res.status(500).json({
-            message: 'Fetching user failed',
-            err: err
-          })
+          return res.status(500).json({message: 'Fetching user failed', err: err})
         }
         if (!doc) {
           return res.status(404).json({
             title: 'User not found',
-            error: {message: 'The user was not found'}
+            error: {
+              message: 'The user was not found'
+            }
           })
         }
-        if(!shared.isCurentUserHasAccess(doc, nameObject, 'read')) {
+        if (!shared.isCurentUserHasAccess(doc, nameObject, 'read')) {
           return res.status(404).json({
             title: 'No rights',
-            error: {message: 'No rights'}
+            error: {
+              message: 'No rights'
+            }
           })
         }
         if (doc) {
@@ -61,113 +58,95 @@ router.use('/', function (req, res, next) {
   })
 })
 
-
-
-router.put('/:id', function (req, res, next) {
-  if(!shared.isCurentUserHasAccess(req.user, nameObject, 'write')) {
-    return res.status(404).json({ title: 'No rights', error: {message: 'No rights'} })
+router.put('/:id', function(req, res, next) {
+  if (!shared.isCurentUserHasAccess(req.user, nameObject, 'write')) {
+    return res.status(404).json({
+      title: 'No rights',
+      error: {
+        message: 'No rights'
+      }
+    })
   }
-  Project.findById(({_id: req.params.id}), function (err, item) {
+  Project.findById(({_id: req.params.id}), function(err, item) {
     if (err) {
-      return res.status(404).json({
-        message: '',
-        err: err
-      })
+      return res.status(404).json({message: '', err: err})
     } else {
       //console.log(req.body)
-        item.ownerCompanies = req.user.companies
-        item.details = req.body.details
-        item.forms = req.body.forms
-        item.status = req.body.status
-        item.embed = req.body.embed
-        item.categories = req.body.categories
-        item.clients = req.body.clients
-        item.quotes = req.body.quotes
-        item.categorie = req.body.categorie
-        item.assignedTos = req.body.assignedTos
-        item.bucketTasks = req.body.bucketTasks
-        item.progressTasks = req.body.progressTasks
-        item.dateProject = req.body.dateProject
+      item.ownerCompanies = req.user.companies
+      item.details = req.body.details
+      item.forms = req.body.forms
+      item.status = req.body.status
+      item.embed = req.body.embed
+      item.categories = req.body.categories
+      item.clients = req.body.clients
+      item.quotes = req.body.quotes
+      item.categorie = req.body.categorie
+      item.assignedTos = req.body.assignedTos
+      item.bucketTasks = req.body.bucketTasks
+      item.progressTasks = req.body.progressTasks
+      item.dateProject = req.body.dateProject
 
-
-
-        item.save(function (err, result) {
-          if (err) {
-            return res.status(404).json({
-              message: 'There was an error, please try again',
-              err: err
-            })
-          }
-          res.status(201).json({
-            message: 'Updated successfully',
-            obj: result
-          })
-        })
+      item.save(function(err, result) {
+        if (err) {
+          return res.status(404).json({message: 'There was an error, please try again', err: err})
+        }
+        res.status(201).json({message: 'Updated successfully', obj: result})
+      })
 
     }
   })
 })
 
-router.post('/', function (req, res, next) {
-  if(!shared.isCurentUserHasAccess(req.user, nameObject, 'write')) {
-    return res.status(404).json({ title: 'No rights', error: {message: 'No rights'} })
-  }
-  if(!req.user.companies.length) {
+router.post('/', function(req, res, next) {
+  if (!shared.isCurentUserHasAccess(req.user, nameObject, 'write')) {
     return res.status(404).json({
-      message: 'You must belong to a companie',
-      err: ''
+      title: 'No rights',
+      error: {
+        message: 'No rights'
+      }
     })
   }
-
+  if (!req.user.companies.length) {
+    return res.status(404).json({message: 'You must belong to a companie', err: ''})
+  }
 
   var project = new Project(req.body)
   project.ownerCompanies = req.user.companies
 
-  project.save(function (err, result) {
+  project.save(function(err, result) {
     if (err) {
       console.log(err)
       return res.status(403).json({
         title: 'There was an issue',
-        error: {message: 'Error'}
+        error: {
+          message: 'Error'
+        }
       })
     }
-    res.status(200).json({
-      message: 'Registration Successfull',
-      obj: result
-    })
+    res.status(200).json({message: 'Registration Successfull', obj: result})
   })
 })
 
-
-
-
 // get all forms from database
-router.get('/page/:page', function (req, res, next) {
+router.get('/page/:page', function(req, res, next) {
   var itemsPerPage = 6
   var currentPage = Number(req.params.page)
   var pageNumber = currentPage - 1
   var skip = (itemsPerPage * pageNumber)
 
-  let searchQuery = {
+  let searchQuery = {}
+  searchQuery['ownerCompanies'] = req.user.ownerCompanies
 
-  }
-
-  if(req.query.search)
+  if (req.query.search)
     searchQuery['details.name'] = new RegExp(req.query.search, 'i')
 
-
-  if(req.query.userId)
+  if (req.query.userId)
     searchQuery['clients'] = mongoose.Types.ObjectId(req.query.userId)
 
-  // console.log(hasWhatsNewCateg)
+    // console.log(hasWhatsNewCateg)
   // console.log(searchQuery)
 
-  Project
-  .find(searchQuery)
-  .sort('-createdAt')
-  .populate({path: 'clients', model: 'User'})
-
-  .populate({ path: 'assignedTos', model: 'User'})
+  Project.find(searchQuery).sort('-createdAt').populate({path: 'clients', model: 'User'}).populate({path: 'assignedTos', model: 'User'})
 
   // .populate({path: 'quotes', model: 'Quote'})
   // .populate(
@@ -175,24 +154,16 @@ router.get('/page/:page', function (req, res, next) {
   //     path: 'bucketTasks.tasks.assignedTos',
   //     model: 'User',
   //   })
-  .limit(itemsPerPage)
-  .skip(skip)
-  .exec(function (err, item) {
+    .limit(itemsPerPage).skip(skip).exec(function(err, item) {
     if (err) {
-      return res.status(404).json({
-        message: 'No results',
-        err: err
-      })
+      return res.status(404).json({message: 'No results', err: err})
     } else {
-      Project
-      .find(searchQuery)
-      .count()
-      .exec(function (err, count) {
-      res.status(200).json({
-          paginationData : {
+      Project.find(searchQuery).count().exec(function(err, count) {
+        res.status(200).json({
+          paginationData: {
             totalItems: count,
-            currentPage : currentPage,
-            itemsPerPage : itemsPerPage
+            currentPage: currentPage,
+            itemsPerPage: itemsPerPage
           },
           data: item
         })
@@ -201,96 +172,72 @@ router.get('/page/:page', function (req, res, next) {
   })
 })
 
-
-
-
-
-
-
-
-
-router.get('/unwind', function (req, res, next) {
-  let searchQuery = {}
-  let dateBegin = req.params.year*1 + '-01-01'
-  let dateEnd = req.params.year*1 +1 + '-01-01'
+router.get('/unwind', function(req, res, next) {
 
   let aggregate = []
-  if(req.query.idProject)
-    aggregate.push({ $match: {_id: mongoose.Types.ObjectId(req.query.idProject)}})
+  aggregate.push({
+    $match: {
+      ownerCompanies: req.user.ownerCompanies
+    }
+  })
 
 
-  aggregate.push({ $unwind : "$bucketTasks" })
-  aggregate.push({ $unwind : "$bucketTasks.tasks" })
 
+  if (req.query.idProject)
+    aggregate.push({
+      $match: {
+        _id: mongoose.Types.ObjectId(req.query.idProject)
+      }
+    })
 
-  if(req.query.myTasks === 'true')
-    aggregate.push({ $match: {'bucketTasks.tasks.assignedTos': mongoose.Types.ObjectId(req.user._id)}})
+  aggregate.push({$unwind: "$bucketTasks"})
+  aggregate.push({$unwind: "$bucketTasks.tasks"})
 
-  aggregate.push({ $lookup: {from: 'users', localField: 'bucketTasks.tasks.assignedTos', foreignField: '_id', as: 'bucketTasks.tasks.assignedTos'} })
+  if (req.query.myTasks === 'true')
+    aggregate.push({
+      $match: {
+        'bucketTasks.tasks.assignedTos': mongoose.Types.ObjectId(req.user._id)
+      }
+    })
+  aggregate.push({
+    $lookup: {
+      from: 'users',
+      localField: 'bucketTasks.tasks.assignedTos',
+      foreignField: '_id',
+      as: 'bucketTasks.tasks.assignedTos'
+    }
+  })
 
-
-  Project
-  .aggregate(aggregate)
-  .exec(function (err, item) {
-   if (err) {
-     return res.status(404).json({
-       message: '',
-       err: err
-     })
-   } else {
-     res.status(200).json({
-       message: 'Success',
-       item: item
-     })
-   }
- })
-
+  Project.aggregate(aggregate).exec(function(err, item) {
+    if (err) {
+      return res.status(404).json({message: '', err: err})
+    } else {
+      res.status(200).json({message: 'Success', item: item})
+    }
+  })
 })
 
-
-
-
-
 // getting user forms to display them on front end
-router.get('/:id', function (req, res, next) {
+router.get('/:id', function(req, res, next) {
 
-
-  Project.findById((req.params.id), function (err, obj) {
+  Project.findById((req.params.id), function(err, obj) {
     if (err) {
-      return res.status(500).json({
-        message: 'An error occured',
-        err: err
-      })
+      return res.status(500).json({message: 'An error occured', err: err})
     }
     if (!obj) {
       return res.status(404).json({
         title: 'No obj found',
-        error: {message: 'Obj not found!'}
+        error: {
+          message: 'Obj not found!'
+        }
       })
     }
 
-
-    Project
-    .findById({_id: req.params.id})
-    .populate({path: 'clients', model: 'User'})
-    .populate({path: 'forms', model: 'Form'})
-    .populate({path: 'assignedTos', model: 'User'})
-    .populate(
-      {
-        path: 'bucketTasks.tasks.assignedTos',
-        model: 'User',
-      })
-    .exec(function (err, item) {
+    Project.findById({_id: req.params.id}).populate({path: 'clients', model: 'User'}).populate({path: 'forms', model: 'Form'}).populate({path: 'assignedTos', model: 'User'}).populate({path: 'bucketTasks.tasks.assignedTos', model: 'User'}).exec(function(err, item) {
       if (err) {
-        return res.status(404).json({
-          message: '',
-          err: err
-        })
+        return res.status(404).json({message: '', err: err})
       } else {
-        res.status(200).json({
-          message: 'Success',
-          item: item
-        })
+        res.status(200).json({message: 'Success', item: item})
       }
     })
   })
@@ -328,41 +275,36 @@ router.get('/:id', function (req, res, next) {
 //   })
 // })
 
-
-router.delete('/:id', function (req, res, next) {
-  if(!shared.isCurentUserHasAccess(req.user, nameObject, 'write')) {
-    return res.status(404).json({ title: 'No rights', error: {message: 'No rights'} })
+router.delete('/:id', function(req, res, next) {
+  if (!shared.isCurentUserHasAccess(req.user, nameObject, 'write')) {
+    return res.status(404).json({
+      title: 'No rights',
+      error: {
+        message: 'No rights'
+      }
+    })
   }
-  Project.findById((req.params.id), function (err, item) {
+  Project.findById((req.params.id), function(err, item) {
     if (err) {
-      return res.status(500).json({
-        message: 'An error occured',
-        err: err
-      })
+      return res.status(500).json({message: 'An error occured', err: err})
     }
     if (!item) {
       return res.status(404).json({
         title: 'No form found',
-        error: {message: 'Form not found!'}
+        error: {
+          message: 'Form not found!'
+        }
       })
     }
 
     // deleting the form from the database
-    item.remove(function (err, result) {
+    item.remove(function(err, result) {
       if (err) {
-        return res.status(500).json({
-          title: 'An error occured',
-          error: err
-        })
+        return res.status(500).json({title: 'An error occured', error: err})
       }
-      res.status(200).json({
-        message: 'Item is deleted',
-        obj: result
-      })
+      res.status(200).json({message: 'Item is deleted', obj: result})
     })
   })
 })
-
-
 
 module.exports = router
