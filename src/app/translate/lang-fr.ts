ef4bf980a5b7f1287ba72fabefdcb591395ec4e8
fredfouillet <30604899+fredfouillet@users.noreply.github.com>
2017-08-02 09:53:18 +0200

    Update lang-fr.ts

--- src/app/translate/lang-fr.ts
+++ src/app/translate/lang-fr.ts
@@ -3,14 +3,30 @@
 export const LANG_FR_NAME = 'fr';
 
 export const LANG_FR_TRANS = {
+    
+    'Back': 'Retour',
+    'My Planning': 'Mon Planning',
+    'My profile': 'Mon profil',
+    'My company': 'Mon Entreprise',
+    'Logout': 'Déconnexion',
+    
     'Add a product': 'Ajouter un produit',
     'Create a new': 'Créer un nouveau',
     'product': 'produit',
     'Products': 'Produits',
-    'right': 'droit',
-    'Rights': 'Droits',
-    'Back': 'Retour',
-
+    'Create an new product':, 'Créer un nouveau produit',
+    'List': 'Liste',
+    'Name': 'Nom',
+    'Price without taxes': 'Prix HT',
+    'Product Name': 'Nom du Produit',
+    'Companie': 'Fournisseur',
+    'Height': 'Hauteur',
+    'Width': 'Largeur',
+    'Depth': 'Profondeur',
+    'CostPrice': 'Coût',
+    'Selling Price': 'Prix de vente',
+    'Save': 'Sauvegarder',
+  
     'Quotes': 'Devis',
     'quote': 'devis',
     'project': 'Project',
@@ -19,7 +35,11 @@ export const LANG_FR_TRANS = {
     'task': 'tâche',
     'Tasks': 'Tâches',
     'Add a project': 'Ajouter un project',
-    'Logout': 'Déconexion',
+  
+
+  
+    'right': 'droit',
+    'Rights': 'Droits',
   
     'Create a new quote': 'Creer un nouveau devis',
 
