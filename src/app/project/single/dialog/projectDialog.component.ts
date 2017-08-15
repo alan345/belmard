98713085a6588b7eecc5acc92e588316147c3ee9
alan345 <alan345@gmail.com>
2017-08-15 13:56:12 -0700

    ok

--- src/app/project/single/dialog/projectDialog.component.ts
+++ src/app/project/single/dialog/projectDialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { MdDialogRef} from '@angular/material';
 import { ProjectSingleComponent }  from '../projectSingle.component';
 
