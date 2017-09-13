d358e837231a3cd4f525f8753969ba66590c417a
GuillaumeGooPlus <guillaume@gooplus.fr>
2017-09-13 19:43:27 +0200

    Update quote.js

--- server/routes/quote.js
+++ server/routes/quote.js
@@ -125,14 +125,9 @@ router.get('/pdf/:quoteId', function(req, res, next) {
     },
     "footer": {
     "height": "50px",
-    "contents": {
-      first: 'Cover page',
-      2: 'Second page', // Any page number is working. 1-based index
-      default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-      last: 'Last Page'
-    }
+    
   },
-    "border": "50px",
+    "border": "10px",
   };
   
 
