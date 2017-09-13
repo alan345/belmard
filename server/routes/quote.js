878cb969c44b0bfbee154d1942246991ae2de0d8
GuillaumeGooPlus <guillaume@gooplus.fr>
2017-09-13 19:48:00 +0200

    Update quote.js

--- server/routes/quote.js
+++ server/routes/quote.js
@@ -333,7 +333,7 @@ router.get('/pdf/:quoteId', function(req, res, next) {
 
                  </style>
                  `
-                 html += `<div id="pageHeader" class="col-12">
+                 html += `<div id="pageHeader" class="col-12"> <img class="img" src="http://belmard-renovation.fr/wp-content/uploads/2016/05/BELMARD.png">
                   
                   </div>`
 
