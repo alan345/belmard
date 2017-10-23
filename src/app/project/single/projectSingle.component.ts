9cf94397c9a7b0f55c47e58b54d19b7ba4e4c1ba
alan345 <alan345@gmail.com>
2017-10-23 17:34:54 +0200

    trad

--- src/app/project/single/projectSingle.component.ts
+++ src/app/project/single/projectSingle.component.ts
@@ -16,7 +16,7 @@ import { User } from '../../user/user.model';
 import { Quote } from '../../quote/quote.model';
 import { AuthService} from '../../auth/auth.service';
 import { Search } from '../../shared/shared.model';
-import { TranslateService } from '../../translate/translate.service';
+// import { TranslateService } from '../../translate/translate.service';
 
 
 
@@ -68,15 +68,15 @@ export class ProjectSingleComponent implements OnInit {
     private userService: UserService,
     private quoteService: QuoteService,
     private authService: AuthService,
-    private translateService: TranslateService,
+    // private translateService: TranslateService,
   ) {
   }
 
 
 
-  translate(text: string) {
-    return this.translateService.instant(<string>text)
-  }
+  // translate(text: string) {
+  //   return this.translateService.instant(<string>text)
+  // }
 
   ngOnInit() {
     this.myForm = this._fb.group({
