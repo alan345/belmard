ed09c50ad73cc93172dbffc3c3989011ed086472
alan345 <alan345@gmail.com>
2018-03-07 17:33:06 -0800

    ok

--- src/app/user/users/adminUsers.component.ts
+++ src/app/user/users/adminUsers.component.ts
@@ -123,25 +123,25 @@ export class AdminUsersComponent implements OnInit {
   }
 
   checkIfCanCreateNewTeamUser() {
-    this.createNewTeam = false
-    this.authService.getCurrentUser().rightsInApp.forEach(rightInApp => {
-      rightInApp.detailRight.permissions.forEach(permission => {
-        if (permission.namePermission === 'user') {
-          permission.access.forEach(singleAccess => {
-            if (singleAccess.typeAccess === 'create5') {
-              if(this.fetchedUsers.length < 5) {
-                this.createNewTeam = true
-              }
-            }
-            if (singleAccess.typeAccess === 'create1') {
-              if(this.fetchedUsers.length < 1) {
-                this.createNewTeam = true
-              }
-            }
-          })
-        }
-      })
-    })
+    this.createNewTeam = true
+    // this.authService.getCurrentUser().rightsInApp.forEach(rightInApp => {
+    //   rightInApp.detailRight.permissions.forEach(permission => {
+    //     if (permission.namePermission === 'user') {
+    //       permission.access.forEach(singleAccess => {
+    //         if (singleAccess.typeAccess === 'create5') {
+    //           if(this.fetchedUsers.length < 5) {
+    //             this.createNewTeam = true
+    //           }
+    //         }
+    //         if (singleAccess.typeAccess === 'create1') {
+    //           if(this.fetchedUsers.length < 1) {
+    //             this.createNewTeam = true
+    //           }
+    //         }
+    //       })
+    //     }
+    //   })
+    // })
   }
 
 
