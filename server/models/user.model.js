c6792c802a768e1cda74cfd5a348de1cadcaff1e
alan345 <alan345@gmail.com>
2018-03-27 12:30:11 -0700

    ok

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -53,7 +53,9 @@ var user = new Schema({
         state : {type: String, default: ['']},
         zip : {type: String, default: ['']},
         country : {type: String, default: ['']},
-      }],      
+      }],
+
+            
       // statusHouse:{type: String, default: ['']},
       // otherData:{type: String, default: ['']},
       // detailHouse:{
